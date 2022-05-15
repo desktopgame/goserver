f4a84e965fe367af983883b3996c3fba865de99e
koya <gnndr36@gmail.com>
2022-05-15 14:57:43 +0900

    Update: テンプレートのテスト

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -11,7 +12,8 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/main", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/views/MainPage.html")
+		tmpl := template.Must(template.ParseFiles("./web/views/MainPage.html"))
+		tmpl.Execute(w, "Main")
 	})
 	fs := http.FileServer(http.Dir("web/dist/js/"))
 	r.Handle("/js/*", http.StripPrefix("/js/", fs))
